4a3271807df93c5fcd0e64edf2f0ad718602187f
Phillip Mueller <pmueller32@verizon.net>
2022-12-01 16:18:36 -0600

    remove superfluous code

--- Source.cpp
+++ Source.cpp
@@ -3,15 +3,7 @@ using namespace std;
 
 int main()
 {
-	if(true)
-	{
-		int *a;
-		a = new int;
-		*a = 38;
-
-		cout << "Hello World!" << *a;
-		delete a;
-		return 0;
-	}
+	cout << "Hollow World!";
+	return 0;
 } // main
 
