724934113a4301bc65eade87e95b30604f0fe341
Phillip Mueller <pmueller32@verizon.net>
2022-12-02 09:43:30 -0600

    ensure ofstream closes at scope exit

--- fstream/fstream_test.cpp
+++ fstream/fstream_test.cpp
@@ -2,17 +2,21 @@
 #include <fstream>
 using namespace std;
 
+static void _did_it_close_hf(string const &filepath)
+{
+	ofstream my_file;
+
+	my_file.open(filepath);
+	my_file << "this be a test\n";
+//	my_file.close();
+
+} // _did_it_close_hf
+
+
 int main()
 {
 	string const filename_k = "my_file.txt";
-	ofstream my_file;
-
-	my_file.open(filename_k);
-
-	my_file << "this be a test\n";
-
-	my_file.close();
-
+	_did_it_close_hf(filename_k);
 	cout << "hopefully created " << filename_k << endl;
 
 } // main
