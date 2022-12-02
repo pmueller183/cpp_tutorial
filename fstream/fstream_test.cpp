e8e86f31f1579920d86c3e07d76e2773925081c7
Phillip Mueller <pmueller32@verizon.net>
2022-12-02 09:37:57 -0600

    initial ofstream

--- fstream/fstream_test.cpp
+++ fstream/fstream_test.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int main()
 {
-	cout << "fstream_test\n";
+	string const filename_k = "my_file.txt";
+	ofstream my_file;
+
+	my_file.open(filename_k);
+
+	my_file << "this be a test\n";
+
+	my_file.close();
+
+	cout << "hopefully created " << filename_k << endl;
+
 } // main
 
