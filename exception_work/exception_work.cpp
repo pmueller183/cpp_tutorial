b88fbc6c7a25943d96c98e3690d0502c38ce1e8f
Phillip Mueller <pmueller32@verizon.net>
2022-12-03 14:44:04 -0600

    intermediate

--- exception_work/exception_work.cpp
+++ exception_work/exception_work.cpp
@@ -4,19 +4,43 @@
 #include <iostream>
 using namespace std;
 
+void _try_catch_work_hf()
+{
+	char the_err;
+	cout << "a: int error; b: double error; c: string error; d: other error;"
+			"e: no error";
+	cin >> the_err;
+
+	if('a' == the_err)
+		throw 7;
+	else if('b' == the_err)
+		throw -13.3;
+	else if('c' == the_err)
+		throw "this be an error";
+	else if('d' == the_err)
+		throw false;
+	else
+		; // do nothing
+} // _try_catch_work_hf
+
 int main()
 {
+	bool is_good;
+
+	is_good = false;
 	try 
 	{
-		int const age = 31;
-		if(age >= 18) 
-			cout << "Access granted - you are old enough.";
-		else
-			throw age;
+		is_good = true;
+		_try_catch_work_hf();
 	} // try block
-	catch (int err_num) 
+	catch(int err_num) 
 	{
-		cout << "Access denied - You must be at least 18 years old.\n";
-		cout << "Age is: " << err_num;
+		cout << "int error " << err_num << endl;
 	}
+	catch(...)
+	{
+		cout << "some other error\n";
+	}
+	cout << is_good ? "Good]n" : "bad :-(\n";
+
 } // main
