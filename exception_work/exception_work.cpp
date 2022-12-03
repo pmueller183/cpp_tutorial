d992be0f76377d21e2f9ff8a2d08634f23b8acd0
Phillip Mueller <pmueller32@verizon.net>
2022-12-03 17:13:50 -0600

    did nested errors

--- exception_work/exception_work.cpp
+++ exception_work/exception_work.cpp
@@ -7,8 +7,8 @@ using namespace std;
 void _try_catch_work_hf()
 {
 	char the_err;
-	cout << "a: int error; b: double error; c: string error; d: other error;"
-			"e: no error";
+	cout << "a: int error\nb: double error\nc: string error\nd: other error\n"
+			"e: no error\n";
 	cin >> the_err;
 
 	if('a' == the_err)
@@ -16,31 +16,68 @@ void _try_catch_work_hf()
 	else if('b' == the_err)
 		throw -13.3;
 	else if('c' == the_err)
-		throw "this be an error";
+		throw string("this be an error");
 	else if('d' == the_err)
 		throw false;
+	else if('e' == the_err)
+		int const qq = 77; //do nothing
 	else
-		; // do nothing
+	{
+		string err_str;
+		err_str = "unknown option >";
+		err_str += the_err;
+		err_str += "<";
+		throw err_str;
+	} // bad option
 } // _try_catch_work_hf
+
+void _try_outside_catch_work_hf()
+{
+
+	try
+	{
+		_try_catch_work_hf();
+	}
+	catch(double)
+	{
+		cout << "caught double error; made good\n";
+	}
+	catch(...)
+	{
+		cout << "caught error; rethrowing\n";
+		throw;
+	}
+} // _try_outside_catch_work_hf
 
 int main()
 {
 	bool is_good;
 
-	is_good = false;
-	try 
+	try
 	{
 		is_good = true;
-		_try_catch_work_hf();
+		_try_outside_catch_work_hf();
 	} // try block
-	catch(int err_num) 
+	catch(int int_val) 
 	{
-		cout << "int error " << err_num << endl;
+		is_good = false;
+		cout << "int error " << int_val << endl;
 	}
+	catch(double dbl_val)
+	{
+		is_good = false;
+		cout << "double error " << dbl_val << endl;
+	}
+	catch(string &str_val)
+	{
+		is_good = false;
+		cout << "string error >" << str_val << "<\n";
+	} // catch string
 	catch(...)
 	{
-		cout << "some other error\n";
+		is_good = false;
+		cout << "unknown exception\n";
 	}
-	cout << is_good ? "Good]n" : "bad :-(\n";
+	cout << (is_good ? "Good\n" : "bad :-(\n");
 
 } // main
