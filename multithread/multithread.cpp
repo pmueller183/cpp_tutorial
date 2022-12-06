e164c9cea199f67ff037957c47973298701c9e87
Phillip Mueller <pmueller32@verizon.net>
2022-12-06 12:45:20 -0600

    use mutex to keep various thread cout from running into each other

--- multithread/multithread.cpp
+++ multithread/multithread.cpp
@@ -1,45 +1,56 @@
 // multithread.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include <mutex>
 #include <thread>
 #include <iostream>
 #include <vector>
 #include <string>
+#include <chrono>
 
-static int count_f = 0;
 
-static void _do_some_work_hf()
+static void _do_some_work_hf(int thread_id, std::mutex *cout_guard)
 {
 	int data;
 	std::string scrstr;
 
-	data = count_f;
-	++count_f;
+	data = thread_id;
 
-	std::cout << "the _do_some_work_hf function is running on another thread.\n";
+	{
+		std::lock_guard<std::mutex> lock(*cout_guard);
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		std::cout << "the _do_some_work_hf ";
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		std::cout << "function is running on another ";
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		std::cout << "thread.\n";
+	};
 
-	// pause for a moment to provide a delay to make threads mor apparent
-	std::this_thread::sleep_for(std::chrono::seconds(2));
 	scrstr = std::to_string(data);
-	std::cout << "The function call by worker thread " << scrstr << " has ended.\n";
+	{
+		std::lock_guard<std::mutex> lock(*cout_guard);
+		std::cout << "The function call ";
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		std::cout << "by worker thread " << scrstr << " ";
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		std::cout << "has ended.\n";
+	}
 } // _do_some_work_hf
 
 int main()
 {
 	std::cout << "Hello World!\n";
 	std::vector<std::thread> the_threads;
+	std::mutex cout_guard;
 
 	for(int ii = 0; ii < 10; ++ii)
 	{
-		the_threads.push_back(std::thread(_do_some_work_hf));
-		std::cout << "the main() thread call this after starting the new thread.\n";
+		the_threads.push_back(std::thread(
+				_do_some_work_hf, ii, &cout_guard));
 	} // for ii
 
 	for(auto &ii:the_threads)
-	{
 		ii.join();
-	}
-
 
 } // main
 
