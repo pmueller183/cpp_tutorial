f6e88aee06d465bb92b7c1bda10549a122c3b995
Phillip Mueller <pmueller32@verizon.net>
2022-12-06 16:00:25 -0600

    remove unneeded dual sync classes

--- multithread/multithread.cpp
+++ multithread/multithread.cpp
@@ -29,13 +29,13 @@ public:
 typedef sync_cls *sync_cls_ptr;
 
 static void _do_some_work_hf(int thread_id, std::mutex *cout_guard, 
-		sync_cls *from_main, sync_cls *to_main)
+		sync_cls *the_sync)
 {
 	// don't start until caller sets sync variable to 1
 	{
-		unique_lock_mutex the_lock(from_main->mutex_m);
-		from_main->cv_m.wait(
-				the_lock, [from_main]{return from_main->val_m >= 1;});
+		unique_lock_mutex the_lock(the_sync->mutex_m);
+		the_sync->cv_m.wait(
+				the_lock, [the_sync]{return the_sync->val_m >= 1;});
 	}
 
 	int data;
@@ -56,16 +56,16 @@ static void _do_some_work_hf(int thread_id, std::mutex *cout_guard,
 	// tell caller we finished initializing (sync var 2)
 	// wait until call sets state to 2
 	{
-		lock_guard_mutex the_lock(to_main->mutex_m);
-		to_main->val_m = 2;
-		to_main->cv_m.notify_one();
+		lock_guard_mutex the_lock(the_sync->mutex_m);
+		the_sync->val_m = 2;
+		the_sync->cv_m.notify_one();
 	}
 
 	// wait until call sets state to 3
 	{
-		unique_lock_mutex the_lock(from_main->mutex_m);
-		from_main->cv_m.wait(the_lock, 
-				[from_main]{return from_main->val_m >= 3;});
+		unique_lock_mutex the_lock(the_sync->mutex_m);
+		the_sync->cv_m.wait(the_lock, 
+				[the_sync]{return the_sync->val_m >= 3;});
 	}
 
 	scrstr = std::to_string(data);
@@ -80,9 +80,9 @@ static void _do_some_work_hf(int thread_id, std::mutex *cout_guard,
 
 	// tell caller we're done state 4
 	{
-		lock_guard_mutex the_lock(to_main->mutex_m);
-		to_main->val_m = 4;
-		to_main->cv_m.notify_one();
+		lock_guard_mutex the_lock(the_sync->mutex_m);
+		the_sync->val_m = 4;
+		the_sync->cv_m.notify_one();
 	}
 
 } // _do_some_work_hf
@@ -90,51 +90,48 @@ static void _do_some_work_hf(int thread_id, std::mutex *cout_guard,
 int main()
 {
 	std::vector<int> the_states;
-	std::vector<sync_cls_ptr> from_mains, to_mains;
+	std::vector<sync_cls_ptr> the_syncs;
 	std::vector<std::thread> the_threads;
 	std::mutex cout_guard;
 
 	for(int ii = 0; ii < 10; ++ii)
 	{
-		sync_cls_ptr from_main, to_main;
-		from_main = new sync_cls;
-		from_mains.push_back(from_main);
-		to_main = new sync_cls;
-		to_mains.push_back(to_main);
+		sync_cls_ptr the_sync;
+		the_sync = new sync_cls;
+		the_syncs.push_back(the_sync);
 		the_threads.push_back(
-				std::thread(_do_some_work_hf, ii, &cout_guard, from_main, to_main));
+				std::thread(_do_some_work_hf, ii, &cout_guard, the_sync));
 		the_states.push_back(0);
 	} // for ii
 
 	for(int ii = 9; ii >= 0; --ii)
 	{
-		lock_guard_mutex the_lock(from_mains[ii]->mutex_m);
+		lock_guard_mutex the_lock(the_syncs[ii]->mutex_m);
 		the_states[ii] = 1;
-		from_mains[ii]->val_m = 1;
+		the_syncs[ii]->val_m = 1;
 	}
 
 	while(the_states[0] != 4)
 	{
 		for(int ii = 9; ii >= 0; --ii)
 		{
+			sync_cls *the_sync = the_syncs[ii];
 			{
-				sync_cls *to_main = to_mains[ii];
-				unique_lock_mutex the_lock(to_main->mutex_m);
-				to_main->cv_m.wait_for(the_lock,
+				unique_lock_mutex the_lock(the_sync->mutex_m);
+				the_sync->cv_m.wait_for(the_lock,
 						std::chrono::seconds(0),
-						[to_main]{return to_main->val_m >= 2;});
-				the_states[ii] = to_main->val_m;
+						[the_sync]{return the_sync->val_m >= 2;});
+				the_states[ii] = the_sync->val_m;
 				int const qq = 77;
 			}
 			if(2 == the_states[ii])
 			{
 				if(9 == ii || the_states[ii+1] >= 3)
 				{
-					sync_cls *from_main = from_mains[ii];
-					lock_guard_mutex the_lock(from_main->mutex_m);
+					lock_guard_mutex the_lock(the_sync->mutex_m);
 					the_states[ii] = 3;
-					from_main->val_m = 3;
-					from_main->cv_m.notify_one();
+					the_sync->val_m = 3;
+					the_sync->cv_m.notify_one();
 					std::this_thread::sleep_for(std::chrono::milliseconds(10));
 				}
 			}
