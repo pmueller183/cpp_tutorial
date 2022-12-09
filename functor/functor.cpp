876bd48a39ab16d4aa5840865311734936af2445
Phillip Mueller <pmueller32@verizon.net>
2022-12-09 10:47:32 -0600

    initial functor test

--- functor/functor.cpp
+++ functor/functor.cpp
@@ -3,8 +3,25 @@
 #include <iostream>
 
 using std::cout;
+using std::endl;
+
+struct abs_value_ftr
+{
+	float operator()(float val) { return val >= 0 ? val : -val; }
+	int   operator()(int   val) { return val >= 0 ? val : -val; }
+}; // abs_value_ftr
 
 int main()
 {
-	cout << "Hello World!\n";
-}
+	cout << endl;
+
+	float flt_val;
+	int int_val;
+	abs_value_ftr abs_value;
+
+	flt_val = static_cast<float>(-13.111);
+	int_val = -13;
+	cout << "flt_val " << flt_val << " abs " << abs_value(flt_val) << endl;
+	cout << "int_val " << int_val << " abs " << abs_value(int_val) << endl;
+} // main
+
