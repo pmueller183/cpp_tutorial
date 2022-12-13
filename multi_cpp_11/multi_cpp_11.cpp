d7219613e5823f9921298ae333a0c73707c2ea26
Phillip Mueller <pmueller32@verizon.net>
2022-12-13 13:25:29 -0600

    did bounded_buffer

--- multi_cpp_11/multi_cpp_11.cpp
+++ multi_cpp_11/multi_cpp_11.cpp
@@ -6,6 +6,7 @@
 
 #include <thread>
 #include <mutex>
+typedef std::lock_guard<std::mutex> lock_mutex;
 
 #include <iostream>
 #include <iomanip>
@@ -140,6 +141,33 @@ static void _timed_work_hf(std::once_flag *the_flag, std::mutex *cout_guard, std
 	} // while(true)
 } // _timed_work_hf
 
+void _consumer_hf(int ndx, std::mutex *cout_guard, bounded_buffer_cls *buffer)
+{
+	for(auto ii = 0; ii < 50; ++ii)
+	{
+		int const val = buffer->fetch();
+		{
+			lock_mutex dummy(*cout_guard);
+			cout << "consumer " << ndx << " fetched  " << std::setw(4) << val << endl;	
+		}
+		std::this_thread::sleep_for(std::chrono::milliseconds(50));
+	} // for ii
+} // _consumer_hf
+
+void _producer_hf(int ndx, std::mutex *cout_guard, bounded_buffer_cls *buffer)
+{
+	for(auto ii = 0; ii < 75; ++ii)
+	{
+		int const val = ndx * 100 + ii;
+		buffer->deposit(val);
+		{
+			lock_mutex dummy(*cout_guard);
+			cout << "producer " << ndx << " produced " << std::setw(4) << val << endl;
+		}
+		std::this_thread::sleep_for(std::chrono::milliseconds(20));
+	} // for ii
+} // _producer_hf
+
 int main()
 {
 	{ // startup
@@ -207,6 +235,20 @@ int main()
 		std::this_thread::sleep_for(std::chrono::milliseconds(sleep_mlls_kf));
 		cout << "hopefully done\n\n";
 	} // timed_work
+
+	{ // consumer / producer
+		std::mutex cout_guard;
+		bounded_buffer_cls buffer(20);
+
+		std::thread c0(_consumer_hf, 0, &cout_guard, &buffer);
+		std::thread c1(_consumer_hf, 1, &cout_guard, &buffer);
+		std::thread c2(_consumer_hf, 2, &cout_guard, &buffer);
+		std::thread p0(_producer_hf, 0, &cout_guard, &buffer);
+		std::thread p1(_producer_hf, 1, &cout_guard, &buffer);
+
+		c0.join(); c1.join(); c2.join(); p0.join(); p1.join();
+		
+	} // consumer / producer
 
 } // main
 
