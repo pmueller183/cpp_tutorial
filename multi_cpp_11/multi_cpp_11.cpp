f3a7eac4d90fa7fe94120ff94f98505be4c0d868
Phillip Mueller <pmueller32@verizon.net>
2022-12-13 09:42:37 -0600

    multi -- messages are mixed

--- multi_cpp_11/multi_cpp_11.cpp
+++ multi_cpp_11/multi_cpp_11.cpp
@@ -1,13 +1,24 @@
 //345678911234567892123456789312345678941234567895123456789612345678971234567898123456-
 
+#include <vector>
+#include <thread>
 #include <iostream>
+
+typedef std::vector<std::thread> thread_vec;
 
 using std::cout;
 using std::endl;
 
+static void _hello_hf()
+{
+	cout << "Hello from thread " << std::this_thread::get_id() << endl;;
+} // _hello_hf
+
 int main()
 {
-	cout << "Hello World!\n";
+	thread_vec the_threads;
+	for(auto ii = 0; ii < 5; ++ii)
+		the_threads.push_back(std::thread(_hello_hf));
+	for(auto &ii: the_threads)
+		ii.join();
 } // main
-
-
