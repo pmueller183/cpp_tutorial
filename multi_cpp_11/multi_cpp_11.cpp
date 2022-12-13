e344838f8663bd6cd0fbc50c203981d129f69946
Phillip Mueller <pmueller32@verizon.net>
2022-12-13 10:33:26 -0600

    recursive_mutex

--- multi_cpp_11/multi_cpp_11.cpp
+++ multi_cpp_11/multi_cpp_11.cpp
@@ -74,22 +74,29 @@ void _counter_hf(counter_sct *val)
 
 struct cpx_sct
 {
-	std::mutex mutex_m;
+	std::recursive_mutex mutex_m;
 	int val_m;
 
 	cpx_sct() : val_m(100) {}
 
 	void mul(int x)
 	{
-		std::lock_guard<std::mutex> lock(mutex_m);
+		std::lock_guard<std::recursive_mutex> lock(mutex_m);
 		val_m *= x;
 	} // mul
 
 	void div(int x)
 	{
-		std::lock_guard<std::mutex> lock(mutex_m);
+		std::lock_guard<std::recursive_mutex> lock(mutex_m);
 		val_m /= x;
 	} // div
+
+	void muldiv(int x, int y)
+	{
+		std::lock_guard<std::recursive_mutex> lock(mutex_m);
+		mul(x);
+		div(y);
+	} // muldiv
 }; // cpx_sct
 
 int main()
@@ -128,9 +135,16 @@ int main()
 
 	{ // complex
 		cpx_sct the_cpx;
+		int backup_val;
+		backup_val = the_cpx.val_m;
 
+		the_cpx.val_m = backup_val;
 		the_cpx.mul(10);
 		the_cpx.div(20);
+		cout << "the_cpx " << the_cpx.val_m << endl;
+
+		the_cpx.val_m = backup_val;
+		the_cpx.muldiv(10, 20);
 		cout << "the_cpx " << the_cpx.val_m << endl;
 
 		std::this_thread::sleep_for(std::chrono::milliseconds(sleep_mlls_kf));
