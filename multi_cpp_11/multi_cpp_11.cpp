9ef069803ccb07b33903e370927d0f6094b6fcd7
Phillip Mueller <pmueller32@verizon.net>
2022-12-13 09:36:24 -0600

    start multi_cpp_11

---
+++ multi_cpp_11/multi_cpp_11.cpp
@@ -0,0 +1,13 @@
+//345678911234567892123456789312345678941234567895123456789612345678971234567898123456-
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+	cout << "Hello World!\n";
+} // main
+
+
