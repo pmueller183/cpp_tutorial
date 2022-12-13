b2af413eb3cc68033a71abf94cc05e11b6379fb8
Phillip Mueller <pmueller32@verizon.net>
2022-12-13 10:22:18 -0600

    add exceptions and catching

--- multi_cpp_11/multi_cpp_11.cpp
+++ multi_cpp_11/multi_cpp_11.cpp
@@ -1,16 +1,36 @@
 //345678911234567892123456789312345678941234567895123456789612345678971234567898123456-
 
 #include <vector>
+
+#include <random>
+
 #include <thread>
 #include <mutex>
+
 #include <iostream>
+using std::cout;
+using std::endl;
+
+static void _make_rnd_eng_hf(std::mt19937 *rnd_eng)
+{
+	std::random_device rnd_dev;
+	std::seed_seq rnd_seed{rnd_dev(), rnd_dev(), rnd_dev(), rnd_dev(),
+			rnd_dev(), rnd_dev(), rnd_dev(), rnd_dev()};
+
+	*rnd_eng = std::mt19937(rnd_seed);
+} // _make_rnd_eng_hf
+
+template<class rnd_generator>
+static int _uniform_int_hf(rnd_generator &the_rnd, int min, int max)
+{
+	std::uniform_int_distribution<int> distribution(min,max);
+	return distribution(the_rnd);
+} // _uniform_int_hf
+
 
 int const sleep_mlls_kf = 100;
 
 typedef std::vector<std::thread> thread_vec;
-
-using std::cout;
-using std::endl;
 
 static void _hello_hf()
 {
@@ -21,21 +41,34 @@ struct counter_sct
 {
 	std::mutex mutex_m;
 	int val_m;
-	counter_sct() : val_m(0){}
-	void increment()
+	int num_exceptions_m;
+	counter_sct() : val_m(0), num_exceptions_m(0) {}
+	template<class rnd_generator>
+	void increment(rnd_generator &the_rnd)
 	{
-		mutex_m.lock();
+		std::lock_guard<std::mutex> lock(mutex_m);
+		if(0 == _uniform_int_hf(the_rnd, 0, 8))
+			throw std::exception("ha ha failed");
 		++val_m;
-		mutex_m.unlock();
 	}
 }; // counter_sct
 
 void _counter_hf(counter_sct *val)
 {
+	std::mt19937 rnd_eng;
+	_make_rnd_eng_hf(&rnd_eng);
+
 	for(int ii = 0; ii < 20; ++ii)
 	{
-		val->increment();
-		std::this_thread::sleep_for(std::chrono::milliseconds(1));
+		try
+		{
+			val->increment(rnd_eng);
+			std::this_thread::sleep_for(std::chrono::milliseconds(1));
+		}
+		catch(std::exception &)
+		{
+			++val->num_exceptions_m;
+		}
 	}
 } // _counter_hf
 
@@ -65,7 +98,8 @@ int main()
 			the_threads.push_back(std::thread(_counter_hf, &the_counter));
 		for(auto &ii : the_threads)
 			ii.join();
-		cout << "val " << the_counter.val_m << endl;
+		cout << "val " << the_counter.val_m << "; exceptions caught " <<
+				the_counter.num_exceptions_m << endl;
 
 		std::this_thread::sleep_for(std::chrono::milliseconds(sleep_mlls_kf));
 		cout << "hopefully done\n\n";
