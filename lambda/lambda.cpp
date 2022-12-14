6c9b2c3c7f0efd02adfee0db865e04760ddf9912
Phillip Mueller <pmueller32@verizon.net>
2022-12-14 10:59:34 -0600

    std::distance with const_iterator

--- lambda/lambda.cpp
+++ lambda/lambda.cpp
@@ -8,13 +8,14 @@ using std::endl;
 typedef std::vector<int> int_vec;
 
 #include <algorithm> // for_each
+#include <iterator>
 
 void _print_vec_hf(int_vec const &the_vec)
 {
 #if 1
 	// lambda
 	std::for_each(the_vec.begin(), the_vec.end(),
-			[](int ii) { cout << ii << " "; }
+			[](int ii) {cout << ii << " ";}
 			);
 #else
 	// old
@@ -26,33 +27,42 @@ void _print_vec_hf(int_vec const &the_vec)
 
 int main()
 {
-	int_vec the_vec = {4, 1, 3, 5, 2, 3, 1, 7};
+	int_vec the_vec = {4, 1, 3, 5, 3, 7, 2, 3, 1, 7};
+	int_vec::const_iterator the_vec_ptr;
 	_print_vec_hf(the_vec);
 
 	// below snippet find first number greater than 4
 	// find_if searches for an element for which
 	// function (third argument) returns true
-	int_vec::const_iterator this_one = std::find_if(
-			the_vec.begin(), the_vec.end(), 
-					[](int ii) { return ii > 4; }
+	the_vec_ptr = std::find_if(the_vec.begin(), the_vec.end(), 
+					[](int ii) {return ii > 4;}
 					);
-	cout << "First number greater than 4 is : " << *this_one << endl;
+	cout << "First number greater than 4 is : " << *the_vec_ptr << endl;
 
 	// function to sort vector; lambda expression is for sorting in
 	// non-increasing order.  Compiler can make out return type as
 	// bool, but shown here just for explanation.
 	std::sort(the_vec.begin(), the_vec.end(), 
-			[](int ii, int jj) -> bool { return ii > jj; }
+			[](int ii, int jj) -> bool {return ii > jj;}
 			);
 	_print_vec_hf(the_vec);
 
 	// function to count numbers greater than or equal to 5
 	auto count_5 = std::count_if(
 			the_vec.begin(), the_vec.end(),
-			[](int ii) {return ii >= 5; }
+			[](int ii) {return ii >= 5;}
 			);
 	cout << "The number of elements greater than or equal to 5 is " <<
 			count_5 << endl;
+
+	_print_vec_hf(the_vec);
+	// function for removing duplicate element (note: std::unique needs 
+	// sorting for all duplicates next to each other
+	the_vec_ptr = std::unique(the_vec.begin(), the_vec.end(),
+			[](int ii, int jj) { return ii == jj; }
+			);
+	the_vec.resize(std::distance(the_vec.cbegin(), the_vec_ptr));
+	_print_vec_hf(the_vec);
 
 	return 0;
 } // main
